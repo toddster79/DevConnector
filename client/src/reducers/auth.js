589666d34e10d76fbb2741f9eab38de4e1697391
Todd Carlson <todd.carlson3030@me.com>
2020-10-14 17:46:51 -0600

    created user-loaded action

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,8 +1,8 @@
-//const { REGISTER_SUCCESS, REGISTER_FAIL } = require("../actions/types")
-
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
 } from '../actions/types'
 
 const initialState = {
@@ -16,6 +16,13 @@ export default function(state = initialState, action) {
     const { type, payload } = action;
 
     switch(type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            }
         case REGISTER_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
@@ -25,6 +32,7 @@ export default function(state = initialState, action) {
                 loading: false
             }
         case REGISTER_FAIL:
+        case AUTH_ERROR:
             localStorage.removeItem('token');
             return {
                 ...state,
@@ -32,7 +40,6 @@ export default function(state = initialState, action) {
                 isAuthenticated: false,
                 loading: false
             }
-
         default:
             return state;
     }
