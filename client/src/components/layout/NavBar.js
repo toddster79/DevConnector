9b87acc4e4528aa1f598c81cf7e768d491197c98
Todd Carlson <todd.carlson3030@me.com>
2020-10-27 13:36:13 -0600

    created dashboard-link

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -8,6 +8,12 @@ const NavBar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
         <li>
+            <Link to="/dashboard">
+            <i className="fas fa-user"></i>{' '}
+            <span className="hide-sm">Dashboard</span>
+              </Link>
+          </li>
+        <li>
           <a onClick={logout} href="#!">
             <i className="fas fa-sign-out-alt"></i>{' '}
             <span className="hide-sm">Logout</span>
@@ -19,8 +25,7 @@ const NavBar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
           <li>
-            <a href="#!">Developers
-            </a>
+            <a href="#!">Developers</a>
             </li>
           <li>
             <Link to="/register">Register</Link>
